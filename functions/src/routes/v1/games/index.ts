43f0197dd52d24a340df70aa988ccd97f01e37b5
smart0815 <michaelho03227@gmail.com>
2025-04-27 15:22:01 +0100

    update routes to expose new endpoints

--- functions/src/routes/v1/games/index.ts
+++ functions/src/routes/v1/games/index.ts
@@ -1,9 +1,48 @@
 import { wrapAsync, createRouter } from '../../../utils/index.js';
-import { getGames } from '../../../apis/firestore/games.js';
+import { 
+  getGames, 
+  getGameById, 
+  createGame, 
+  updateGame, 
+  deleteGame 
+} from '../../../apis/firestore/games.js';
+import { type Game } from '../../../types/index.js';
 
 export const gamesRouter = createRouter();
 
+// Get all games
 gamesRouter.get(
   '/',
   wrapAsync(() => getGames()),
+);
+
+// Get a specific game by ID
+gamesRouter.get(
+  '/:id',
+  wrapAsync((req) => getGameById(req.params.id)),
+);
+
+// Create a new game
+gamesRouter.post(
+  '/',
+  wrapAsync((req) => {
+    const gameData = req.body as Omit<Game, 'id'> & { id?: string };
+    return createGame(gameData);
+  }),
+);
+
+// Update a game
+gamesRouter.put(
+  '/:id',
+  wrapAsync((req) => {
+    const id = req.params.id;
+    const gameData = req.body as Partial<Game>;
+    return updateGame(id, gameData);
+  }),
+);
+
+// Delete a game
+gamesRouter.delete(
+  '/:id',
+  wrapAsync((req) => deleteGame(req.params.id)),
 );
